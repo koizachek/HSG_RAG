7561b205f8ac15ad352130ed1405060c4620abeb
AdnanNezovic <adnannoah2001@gmail.com>
2026-01-17 19:51:28 +0100

    Check CACHE_ENABLED in Cache instead of ChatBotApplication

--- src/cache/cache.py
+++ src/cache/cache.py
@@ -1,10 +1,12 @@
 from .cache_strategies import RedisCache, LocalCache
-from config import CACHE_STRATEGY
+from config import CACHE_STRATEGY, CACHE_ENABLED
 
 class Cache:
     
     @staticmethod
     def get_cache():
+        if not CACHE_ENABLED:
+            return None
         if CACHE_STRATEGY == 'REDIS':
             return RedisCache()
         elif CACHE_STRATEGY == 'LOCAL':
