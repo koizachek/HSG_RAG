3b19337e2e7885d4d01d576a243c3a96e4c2447f
Pygmales <uk084094@student.uni-kassel.de>
2026-05-11 13:16:17 +0200

    more changes to scraping

--- src/const/page_blacklist.py
+++ src/const/page_blacklist.py
@@ -1,5 +1,6 @@
 PAGE_BLACKLIST = [
     'cookie', 'cookies', 'privacy', 'datenschutz', 'popup', 'download',
     'cookie-policy', 'privacy-policy', 'cookie-and-privacy-policy',
-    'data-protection', 'impressum', 'legal', 'terms', 'agb', 'imprint'
+    'data-protection', 'impressum', 'legal', 'terms', 'agb', 'imprint',
+    'mp4', 'pdf', 'interview',
 ]
