6131e6be0963e7cdd684a41163ffb242ba8ece26
koizachek <kozachekdiana@gmail.com>
2026-05-21 17:44:15 +0200

    Fix chat interface message format

--- src/apps/chat/app.py
+++ src/apps/chat/app.py
@@ -91,6 +91,7 @@ class ChatbotApplication:
                     additional_inputs=[agent_state],
                     additional_outputs=[agent_state],
                     title="Executive Education Adviser",
+                    type="messages",
                 )
 
             booking_widget = gr.HTML(
